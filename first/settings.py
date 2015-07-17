f7cb165ddeb462b515264e1427537e25966ea4e7
raviteja <ravi@gmail.com>
2015-07-17 12:38:26 +0530

    my first commit

--- first/settings.py
+++ first/settings.py
@@ -1,3 +1,13 @@
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 """
 Django settings for first project.
 
@@ -100,7 +110,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 from os.path import join
 TEMPLATE_DIRS = (
