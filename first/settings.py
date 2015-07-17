0f746d2da7de48d6832291b1c7d7df500a47ffaf
raviteja <ravi@gmail.com>
2015-07-17 13:52:52 +0530

    my first commit

--- first/settings.py
+++ first/settings.py
@@ -1,13 +1,3 @@
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-
 """
 Django settings for first project.
 
@@ -88,7 +78,7 @@ WSGI_APPLICATION = 'first.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db'),
     }
 }
 
